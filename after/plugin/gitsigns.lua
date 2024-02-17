48d3a2b0803357c3ef7b709fe0ce732ddf0300f9
Jacob Lorenzen <jacob.lorenzen@velux.com>
2024-02-17 18:59:51 +0100

    chore: show line blame and view changes

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,1 +1,14 @@
-require('gitsigns').setup()
+require('gitsigns').setup({
+    current_line_blame = true,
+    on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+
+        map('n', '<leader>gp', gs.preview_hunk)
+    end
+})
