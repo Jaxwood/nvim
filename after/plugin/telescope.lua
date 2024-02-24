4f21207f895091f8e03fca606ef5089362ae8c23
Jacob Lorenzen <jacob.lorenzen@velux.com>
2024-02-24 08:36:18 +0100

    chore: search in .github folder

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,20 @@
+require('telescope').setup({
+    pickers = {
+        live_grep = {
+            additional_args = function(opts)
+                return {
+                    "--hidden",
+                    "--glob", "!**/.git/*",
+                    "--glob", "!**/.cache/*",
+                    "--glob", "!**/yarn.lock"
+                }
+            end
+        },
+    },
+})
+
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-f>', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<C-f>', builtin.git_files, {})
 vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
 vim.keymap.set('n', '<C-y>', builtin.oldfiles, {})
