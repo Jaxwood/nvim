e5ae713418fb68b54bb595ddb34b4b7ebe8581b0
Jacob Lorenzen <jacob.lorenzen@velux.com>
2024-03-05 09:01:58 +0100

    chore: add whichkey

--- lua/jlo/packer.lua
+++ lua/jlo/packer.lua
@@ -6,6 +6,18 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
+    use {
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+            require("which-key").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
 
     use 'xiyaowong/transparent.nvim'
     use 'prichrd/netrw.nvim'
