6d47ec3e01707f4be1387c471389b5ece6fada64
Jacob Lorenzen <jacob.lorenzen@velux.com>
2024-03-05 09:00:19 +0100

    chore: remove unused keymaps

--- after/plugin/floaterm.lua
+++ after/plugin/floaterm.lua
@@ -2,5 +2,3 @@ vim.g.floaterm_width = 0.95
 vim.g.floaterm_height = 0.95
 
 vim.keymap.set('n', '<leader>g', ':FloatermNew lazygit<CR>')
-vim.keymap.set('n', '<leader>k', ':FloatermNew k9s<CR>')
-vim.keymap.set('n', '<leader>m', ':FloatermNew spt<CR>')
