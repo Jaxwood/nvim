5c375fbcf04db6e2a17ffdbe3bc3ec3fd247f721
Jacob Lorenzen <jacob.lorenzen@velux.com>
2025-02-25 09:59:07 +0100

    chore(nvim): remap telescope keymaps

--- lua/jlo/plugins/telescope.lua
+++ lua/jlo/plugins/telescope.lua
@@ -4,14 +4,14 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
 		require("telescope").setup()
+		local builtin = require("telescope.builtin")
 
 		-- set keymaps
 		local keymap = vim.keymap
 
-		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-		keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Fuzzy find recent files" })
-		keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find string in cwd" })
-		keymap.set("n", "<leader>fs", "<cmd>Telescope git_status<cr>", { desc = "Find string under cursor in cwd" })
-		keymap.set("n", "<leader>fc", "<cmd>Telescope git commits<cr>", { desc = "Find todos" })
+		keymap.set("n", "<C-y>", builtin.find_files, {})
+		keymap.set("n", "<C-f>", builtin.git_files, {})
+		keymap.set("n", "<C-g>", builtin.live_grep, {})
+		keymap.set("n", "<C-p>", builtin.oldfiles, {})
 	end,
 }
