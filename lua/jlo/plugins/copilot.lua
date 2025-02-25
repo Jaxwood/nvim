264a189094663598178b1949b47d097a1b312e92
Jacob Lorenzen <jacob.lorenzen@velux.com>
2025-02-25 09:58:54 +0100

    chore(nvim): add copilot

---
+++ lua/jlo/plugins/copilot.lua
@@ -0,0 +1,9 @@
+return {
+	"github/copilot.vim",
+	config = function()
+		vim.g.copilot_no_tab_map = true
+		vim.g.copilot_assume_mapped = true
+
+		vim.keymap.set("i", "<C-j>", 'copilot#Accept("")', { silent = true, expr = true, replace_keycodes = false })
+	end,
+}
