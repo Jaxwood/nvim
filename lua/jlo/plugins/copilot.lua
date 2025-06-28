6f359a4dfc61842f4ebbafbdd5d7e9da6cdbd0a0
Jacob Lorenzen <jacob@lorenzen.me>
2025-06-28 10:01:29 +0200

    chore: switch to codecompanion from copilotchat

--- lua/jlo/plugins/copilot.lua
+++ lua/jlo/plugins/copilot.lua
@@ -5,8 +5,6 @@ return {
     vim.g.copilot_assume_mapped = true
 
     vim.keymap.set("i", "<C-j>", 'copilot#Accept("")', { silent = true, expr = true, replace_keycodes = false })
-    vim.keymap.set("n", "<leader>t", function()
-      require("CopilotChat").open()
-    end)
+    vim.keymap.set("n", "<leader>t", "<cmd>CodeCompanionChat<cr>")
   end,
 }
