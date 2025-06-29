1fa038740ed348dbd09cb191df71b38244deb22e
Jacob Lorenzen <jacob@lorenzen.me>
2025-06-29 11:57:37 +0200

    chore(copilot): add keybind for visual mode

--- lua/jlo/plugins/copilot.lua
+++ lua/jlo/plugins/copilot.lua
@@ -5,6 +5,6 @@ return {
     vim.g.copilot_assume_mapped = true
 
     vim.keymap.set("i", "<C-j>", 'copilot#Accept("")', { silent = true, expr = true, replace_keycodes = false })
-    vim.keymap.set("n", "<leader>t", "<cmd>CodeCompanionChat<cr>")
+    vim.keymap.set({"n", "v" }, "<leader>t", "<cmd>CodeCompanionChat<cr>")
   end,
 }
