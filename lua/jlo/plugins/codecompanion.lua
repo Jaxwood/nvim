c3f2d916b8ff33c2df56feb5b409cb9fd958383b
Jacob Lorenzen <jacob@lorenzen.me>
2025-07-08 18:43:53 +0200

    chore(codecompanion): enable mcphub
    Signed-off-by: Jacob Lorenzen <jacob@lorenzen.me>

--- lua/jlo/plugins/codecompanion.lua
+++ lua/jlo/plugins/codecompanion.lua
@@ -6,4 +6,14 @@ return {
     "nvim-treesitter/nvim-treesitter",
     "ravitemer/mcphub.nvim"
   },
+  extensions = {
+    mcphub = {
+      callback = "mcphub.extensions.codecompanion",
+      opts = {
+        show_result_in_chat = true, -- Show mcp tool results in chat
+        make_vars = true,           -- Convert resources to #variables
+        make_slash_commands = true, -- Add prompts as /slash commands
+      }
+    }
+  },
 }
