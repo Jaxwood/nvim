5ebde468c1cdcf4cb057682101942c75ac515016
Jacob Lorenzen <jacob@lorenzen.me>
2025-07-17 12:40:32 +0200

    chore(codecompanion): set model
    Signed-off-by: Jacob Lorenzen <jacob@lorenzen.me>

--- lua/jlo/plugins/codecompanion.lua
+++ lua/jlo/plugins/codecompanion.lua
@@ -1,12 +1,20 @@
 return {
   "olimorris/codecompanion.nvim",
   opts = {
+    strategies = {
+      chat = {
+        adapter = {
+          name = "copilot",
+          model = "claude-sonnet-4",
+        },
+      },
+    },
     extensions = {
       mcphub = {
         callback = "mcphub.extensions.codecompanion",
         opts = {
           show_result_in_chat = true, -- Show mcp tool results in chat
-          make_vars = true,         -- Convert resources to #variables
+          make_vars = true,           -- Convert resources to #variables
           make_slash_commands = true, -- Add prompts as /slash commands
         }
       }
