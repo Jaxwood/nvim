fafbd7f0735fda2378ec08075bf1fdbcca0081ef
Jacob Lorenzen <jacob@lorenzen.me>
2025-07-24 10:37:06 +0200

    chore(lualine): update config
    Signed-off-by: Jacob Lorenzen <jacob@lorenzen.me>

--- lua/jlo/plugins/lualine.lua
+++ lua/jlo/plugins/lualine.lua
@@ -2,6 +2,25 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("lualine").setup()
+    require("lualine").setup({
+      options = {
+        theme = "auto",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = {
+          {
+            "filename",
+            path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+          }
+        },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+    })
   end,
 }
