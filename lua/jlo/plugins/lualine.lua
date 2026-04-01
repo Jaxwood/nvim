aef04478555db60c4a5475a5ee323b463e437d9a
Jacob Lorenzen <jacob.lorenzen@velux.com>
2026-04-01 09:20:19 +0200

    chore(lualine): make status global and show model

--- lua/jlo/plugins/lualine.lua
+++ lua/jlo/plugins/lualine.lua
@@ -1,12 +1,13 @@
 return {
   "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  dependencies = { "nvim-tree/nvim-web-devicons", "franco-ruggeri/codecompanion-lualine.nvim", },
   config = function()
     require("lualine").setup({
       options = {
         theme = "auto",
         component_separators = { left = "", right = "" },
         section_separators = { left = "", right = "" },
+        globalstatus = true,
       },
       sections = {
         lualine_a = { "mode" },
@@ -17,7 +18,7 @@ return {
             path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
           }
         },
-        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_x = { "codecompanion", "encoding", "fileformat", "filetype" },
         lualine_y = { "progress" },
         lualine_z = { "location" },
       },
