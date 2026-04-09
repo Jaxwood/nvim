646aa4f7242b3583554dc669f532fb684a46a616
Jacob Lorenzen <jacob.lorenzen@velux.com>
2026-04-09 09:48:57 +0200

    chore(deps): update

--- lua/jlo/plugins/blink.lua
+++ lua/jlo/plugins/blink.lua
@@ -11,20 +11,12 @@ return {
 			completion = { documentation = { auto_show = false } },
 			sources = {
 				default = { "lsp", "path", "snippets", "buffer", "copilot" },
-				per_filetype = {
-					codecompanion = { "codecompanion" },
-				},
 				providers = {
 					copilot = {
 						name = "copilot",
 						module = "blink-cmp-copilot",
 						score_offset = 100,
 						async = true,
-					},
-					codecompanion = {
-						name = "CodeCompanion",
-						module = "codecompanion.providers.completion.blink",
-						enabled = true,
 					},
 				},
 			},
--- lua/jlo/plugins/codecompanion.lua
+++ lua/jlo/plugins/codecompanion.lua
@@ -1,6 +1,6 @@
 return {
 	"olimorris/codecompanion.nvim",
-	tag = "v19.9.0",
+	tag = "v19.10.0",
 	opts = {
 		adapters = {
 			http = {
